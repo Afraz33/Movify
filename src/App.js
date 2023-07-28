704eba751ea4a0211d6a6b85d4d32de0a505aab3
Afraz <afraz3301@gmail.com>
2023-07-28 22:01:54 +0500

    Navbar and Logo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 function App() {
   return (
-    <div className="container text-center">
-      <h1 className="text-3xl font-bold  underline">Hello world!</h1>
+    <div className="bg-green-800 text-center h-screen w-screen">
+      <Navbar />
+    </div>
+  );
+}
+
+function Navbar() {
+  return (
+    <div className="h-20 w-full flex flex-row bg-green-600 items-center">
+      <Logo>Movify</Logo>
+    </div>
+  );
+}
+
+function Logo({ children }) {
+  return (
+    <div className="items-center items-end logo-container flex flex-row ml-24">
+      <span className="justify-self-end text-5xl mb-5"> &#x1F4F7;</span>
+      <span className="text-4xl text-white font-semibold italic">
+        {children}
+      </span>
     </div>
   );
 }
